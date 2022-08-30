a797385c164cc594e02fe2a0761c989df3c77a6f
Miles <acute0203@gmail.com>
2022-08-30 18:56:32 +0800

    add reply function

--- app.py
+++ app.py
@@ -227,7 +227,8 @@ def getImageMessage(originalContentUrl):
 
 
 def replyMessage(payload):
-    response = {}
+    r = requests.post('https://api.line.me/v2/bot/message/reply', data=json.dumps(payload), headers=HEADER)
+    print(r.content)
     return 'OK'
 
 
