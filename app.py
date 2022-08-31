bb3d3652ff1f2d8ebe6334dacc63bf0754ac4a95
Miles <acute0203@gmail.com>
2022-08-31 14:07:00 +0800

    add name emoji

--- app.py
+++ app.py
@@ -170,8 +170,20 @@ def sendTextMessageToMe():
 def getNameEmojiMessage():
     lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     productId = "5ac21a8c040ab15980c9b43f"
-    name = ""
+    name = "Miles"
     message = dict()
+    message["type"] = "text"
+    message["text"] = "".join("$" for r in range(len(name)))
+    emojis_list = list()
+    for i, nChar in enumerate(name):
+        emojis_list.append(
+            {
+              "index": i,
+              "productId": productId,
+              "emojiId": f"{lookUpStr.index(nChar) + 1 :03}"
+            }
+        )
+    message["emojis"] = emojis_list
     return message
 
 
@@ -241,7 +253,7 @@ def pushMessage(payload):
 def getTotalSentMessageCount():
     r = requests.get('https://api.line.me/v2/bot/message/quota/consumption', headers=HEADER)
     print(r.json())
-    return 0
+    return r.json()['totalUsage']
 
 
 def getTodayCovid19Message():
