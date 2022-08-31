6b67542643861d807bf9435afc9a71a429ecb76c
Miles <acute0203@gmail.com>
2022-08-31 13:53:37 +0800

    add getTodayCovid19Message

--- app.py
+++ app.py
@@ -245,9 +245,11 @@ def getTotalSentMessageCount():
 
 
 def getTodayCovid19Message():
-    date = ""
-    total_count = 0
-    count = 0
+    r = requests.get('https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=3001&limited=BGD', headers=HEADER)
+    data = r.json()[0]
+    date = data['a04']
+    total_count = data['a06']
+    count = data['a05']
     return F"日期：{date}, 人數：{count}, 確診總人數：{total_count}"
 
 
